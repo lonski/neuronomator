0fcc0ed26c58269c7d03aacbadce1e1459aa5a27
Michal Lonski <michal.lonski@nokia.com>
2018-03-17 10:23:51 +0100

    Prompting user for input parameters.

--- src/main/java/pl/lonski/neuronomator/Neuronomator.java
+++ src/main/java/pl/lonski/neuronomator/Neuronomator.java
@@ -1,7 +1,12 @@
 package pl.lonski.neuronomator;
 
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 import static pl.lonski.neuronomator.network.NeuralNetworkParameters.ActivationFunctions.LOGISTIC;
+
+import java.util.List;
+import java.util.Scanner;
 
 import pl.lonski.neuronomator.network.NeuralNetwork;
 import pl.lonski.neuronomator.network.NeuralNetworkParameters;
@@ -9,23 +14,74 @@ import pl.lonski.neuronomator.network.NeuralNetworkParameters;
 public class Neuronomator {
 
 	public static void main(String[] args) {
-		NeuralNetworkParameters params = new NeuralNetworkParameters()
+		System.out.println(":: Neuronomator 6000 ::");
+		System.out.println();
+		printSeparator("Menu");
+		System.out.println("1. Enter network parameters");
+		System.out.println("2. Use example parameter set");
+		System.out.println("0. Exit");
+		System.out.print(">");
+
+		Scanner scanner = new Scanner(System.in);
+		int choose = scanner.nextInt();
+
+		NeuralNetworkParameters params = new NeuralNetworkParameters();
+		switch (choose) {
+		case 1:
+			params = promptUserForParams(scanner);
+			break;
+		case 2:
+			params = getExampleParams();
+			break;
+		default:
+			System.exit(0);
+		}
+
+		System.out.print("Enter maximum number of iterations\n>");
+		int iterations = scanner.nextInt();
+		System.out.print("Enter tolerated error value\n>");
+		params.setToleratedError(scanner.nextDouble());
+
+		printSeparator("Learning");
+		NeuralNetwork nn = new NeuralNetwork(params);
+		nn.learn(iterations);
+
+		printSeparator("Calculated weights");
+		System.out.println(nn.printWeights());
+	}
+
+	private static NeuralNetworkParameters getExampleParams() {
+		return new NeuralNetworkParameters()
 				.setInput(asList(0.05, 0.1))
 				.setExpectedOutput(asList(0.01, 0.99))
 				.setHiddenLayersCount(2)
 				.setHiddenLayerNeuronsCount(3)
 				.setToleratedError(0.001)
 				.setActivationFn(LOGISTIC);
-
-		printLine("Learning");
-		NeuralNetwork nn = new NeuralNetwork(params);
-		nn.learn(1000000);
-
-		printLine("Calculated weights");
-		System.out.println(nn.printWeights());
 	}
 
-	private static void printLine(String msg) {
+	private static NeuralNetworkParameters promptUserForParams(Scanner scanner) {
+		NeuralNetworkParameters params = new NeuralNetworkParameters();
+		params.setActivationFn(LOGISTIC);
+
+		System.out.print("Enter neuron values of input layer (space separated numbers)\n>");
+		scanner.nextLine();
+		params.setInput(readList(scanner));
+		System.out.print("Enter expected neuron values of output layer\n>");
+		params.setExpectedOutput(readList(scanner));
+		System.out.print("Enter number of hidden layers\n>");
+		params.setHiddenLayersCount(scanner.nextInt());
+		System.out.print("Enter amount of neurons in hidden layers\n>");
+		params.setHiddenLayerNeuronsCount(scanner.nextInt());
+
+		return params;
+	}
+
+	private static List<Double> readList(Scanner scanner) {
+		return stream(scanner.nextLine().split(" ")).map(Double::valueOf).collect(toList());
+	}
+
+	private static void printSeparator(String msg) {
 		msg = "[" + msg.replaceAll(" ", "~") + "]";
 		String line = String.format("-----%-80s%n", msg)
 				.replaceAll(" ", "-")
